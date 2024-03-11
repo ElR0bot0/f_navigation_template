36824018c57a4998c6a32f910d990babf1ae9839
R0bot0 <maldonadolosadar@gmail.com>
2024-03-11 10:59:52 -0500

    Navegacion hecha

--- lib/ui/pages/page2.dart
+++ lib/ui/pages/page2.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 class Page2 extends StatelessWidget {
   Page2({super.key});
   // Obtain the name parameter from the route
-  final String? name = 'some name';
+  final String? name = Get.parameters['name'];
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,9 @@ class Page2 extends StatelessWidget {
         actions: [
           IconButton(
               //todo: Implement back navigation logic making sure to clean the navigation stack
-              onPressed: () => null,
+              onPressed: () {
+                Get.offNamed('/page1');
+              },
               icon: const Icon(Icons.logout))
         ],
       ),
@@ -33,7 +35,7 @@ class Page2 extends StatelessWidget {
               children: [
                 ElevatedButton(
                   //todo: Implement navigation to page3a sending the name as a parameter
-                  onPressed: () => null,
+                  onPressed: () => Get.toNamed('/page3a/?name=$name'),
                   child: const Text(
                     'Option A',
                   ),
